f01520fbf364295a328294694c6bfa3130d4ccbb
Beyda Nur PINARBAŞI <beydanur.pinarbasi@gmail.com>
2024-03-05 22:20:14 +0300

    data context içi dolduruldu

---
+++ Cms.Data/Context/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cms.Data.DataContext
+{
+  internal class DataContext: DbContext
+  {
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+
+    }
+    public DbSet<User> User { get; set; }
+  }
+}
