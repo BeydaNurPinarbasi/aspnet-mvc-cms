3f2c314d63f7c9c7bcf3a1aee6940e9bdda41d15
Beyda Nur PINARBAŞI <beydanur.pinarbasi@gmail.com>
2024-03-07 01:52:06 +0300

    controller kısmına geçildi

--- AspNetMvcCms/Controllers/AuthController.cs
+++ AspNetMvcCms/Controllers/AuthController.cs
@@ -4,9 +4,17 @@ namespace Cms.Web.Mvc.Controllers
 {
   public class AuthController : Controller
   {
-    public IActionResult Index()
-    {
-      return View();
-    }
-  }
+		public IActionResult Register()
+		{
+			return View();
+		}
+		public IActionResult Login(string redirectUrl)
+		{
+			return View();
+		}
+		public IActionResult ForgotPassword()
+		{
+			return View();
+		}
+	}
 }
