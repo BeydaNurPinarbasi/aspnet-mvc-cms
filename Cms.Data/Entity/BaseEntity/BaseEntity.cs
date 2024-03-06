00bf377543a33525da5610ad139f56bc5350791d
Beyda Nur PINARBAŞI <beydanur.pinarbasi@gmail.com>
2024-03-06 23:58:27 +0300

    entity base ksımı düzenleme

---
+++ Cms.Data/Entity/BaseEntity/BaseEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cms.Data.Entity.BaseEntity
+{
+  public abstract class BaseEntity : BaseAuditEntity, IEntity
+  {
+
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+  }
+}
+
