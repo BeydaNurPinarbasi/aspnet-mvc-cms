7fe868a4b46c947d48a62352c729c330000abd52
Beyda Nur PINARBAŞI <beydanur.pinarbasi@gmail.com>
2024-03-21 23:00:20 +0300

    data context

--- Cms.Data/Context/DataContext.cs
+++ Cms.Data/Context/DataContext.cs
@@ -10,12 +10,35 @@ namespace Cms.Data.Context
     {
 
     }
-   public DbSet<Blog>Blog{ get; set; }
 
-   public DbSet <Department>Department { get; set; }
 
-    public DbSet<Doctor>Doctor { get; set; }
-    public DbSet<Patient>Patient { get; set; }
+    public DbSet<Appointment> Appointments { get; set; }
+    public DbSet<Admin> Admins { get; set; }
+    public DbSet<AppointmentManager> AppointmentManagers { get; set; }
+    public DbSet<Contact> Contacts { get; set; }
+    public DbSet<Blog> Blogs { get; set; }
+    public DbSet<Department> Departments { get; set; }
+    public DbSet<DepartmentBlog> DepartmentBlogs { get; set; }
+    public DbSet<DepartmentDetail> DepartmentDetails { get; set; }
+    public DbSet<BlogImage> BlogImages { get; set; }
+    public DbSet<BlogComment> BlogComments { get; set; }
+    public DbSet<Doctor> Doctors { get; set; }
+    public DbSet<Patient> Patients { get; set; }
+    public DbSet<DoctorPatient> DoctorPatients { get; set; }
+    public DbSet<Education> Educations { get; set; }
+    public DbSet<Introduction> Introductions { get; set; }
+    public DbSet<WorkingHour> WorkingHours { get; set; }
+    public DbSet<Surgery> Surgeries { get; set; }
+    public DbSet<SurgeryDoctor> SurgeryDoctors { get; set; }
+
+
+
+
+
+
+  
+    
+   
 
   }
 }
